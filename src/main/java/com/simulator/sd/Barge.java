b0cb88c200a1a4d593baf645614df0b2a46e7f22
rhariv <65856846+rhariv@users.noreply.github.com>
2021-10-27 15:54:31 +0200

    Update Barge.java

--- src/main/java/com/simulator/sd/Barge.java
+++ src/main/java/com/simulator/sd/Barge.java
@@ -1,4 +1,29 @@
 package com.simulator.sd;
 
+import com.simulator.state.BargeS;
+
 public class Barge {
+	BargeS etat_barge;
+	int position;
+	public Barge(BargeS etat_barge, int position) {
+		super();
+		this.etat_barge = etat_barge;
+		this.position = position;
+	}
+	
+	
+	public BargeS getEtat_barge() {
+		return etat_barge;
+	}
+	public void setEtat_barge(BargeS etat_barge) {
+		this.etat_barge = etat_barge;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+	
 }
