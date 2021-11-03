d3bc8dcb63dbdf6ab1b4a3a3db5c4c0dc676059e
rhariv <65856846+rhariv@users.noreply.github.com>
2021-11-03 15:21:57 +0100

    ajout classe container_modif

--- src/main/java/com/simulator/sd/Container.java
+++ src/main/java/com/simulator/sd/Container.java
@@ -9,18 +9,18 @@ boolean plein;
  final int poids_container_max = 10000000;
  Integer  position_ContnairT;
  Leg position_containerL;
- public  enum ContainerService{ EN_ATTENTE, EN_TRANSPORT, CHARGEMENT_DECHARGEMENT, EN_PANNE, EN_REPARATION;}
- private ContainerService status;
+ ContainerS containerService;
+ //private ContainerService status;
  Service contenaire_service;
  Terminal terminal_depard;
  Terminal terminal_destination;
  
- public Container(boolean plein, double poid_vide_container, ContainerService status) {
+ public Container(boolean plein, double poid_vide_container, ContainerS status) {
 		super();
 		this.plein = plein;
 		this.poid_vide_container = poid_vide_container;
 		this.position_ContnairT = null;
-		this.status = status.CHARGEMENT_DECHARGEMENT;		
+		this.containerService = status.CHARGEMENT_DECHARGEMENT;		
 		this.position_containerL=null;
 	}
 
@@ -40,26 +40,59 @@ public void setPoid_vide_container(double poid_vide_container) {
 	this.poid_vide_container = poid_vide_container;
 }
 
-public int getPosition_Contnair() {
-	return position_Contnair;
+public Integer getPosition_ContnairT() {
+	return position_ContnairT;
 }
 
-public void setPosition_Contnair(int position_Contnair) {
-	this.position_Contnair = position_Contnair;
+public void setPosition_ContnairT(Integer position_ContnairT) {
+	this.position_ContnairT = position_ContnairT;
 }
 
-public ContainerService getStatus() {
-	return status;
+public Leg getPosition_containerL() {
+	return position_containerL;
 }
 
-public void setStatus(ContainerService status) {
-	this.status = status;
+public void setPosition_containerL(Leg position_containerL) {
+	this.position_containerL = position_containerL;
+}
+
+public ContainerS getContainerService() {
+	return containerService;
+}
+
+public void setContainerService(ContainerS containerService) {
+	this.containerService = containerService;
+}
+
+public Service getContenaire_service() {
+	return contenaire_service;
+}
+
+public void setContenaire_service(Service contenaire_service) {
+	this.contenaire_service = contenaire_service;
+}
+
+public Terminal getTerminal_depard() {
+	return terminal_depard;
+}
+
+public void setTerminal_depard(Terminal terminal_depard) {
+	this.terminal_depard = terminal_depard;
+}
+
+public Terminal getTerminal_destination() {
+	return terminal_destination;
+}
+
+public void setTerminal_destination(Terminal terminal_destination) {
+	this.terminal_destination = terminal_destination;
 }
 
 public int getPoids_container_max() {
 	return poids_container_max;
 }
- 
+
+
  
  
 }
